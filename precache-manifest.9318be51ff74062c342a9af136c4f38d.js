cc127685bf29f23b1086ebe8dedf2fb6428015f1
giorvog <gior.gos21@hotmail.com>
2021-02-02 20:34:29 +0200

    Updates

---
+++ precache-manifest.9318be51ff74062c342a9af136c4f38d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d58fb6763f6d2b217274d263deb6ee7",
+    "url": "/cretanTouristGuide/index.html"
+  },
+  {
+    "revision": "e4dd09d4c367609a34d0",
+    "url": "/cretanTouristGuide/static/css/main.a812dc08.chunk.css"
+  },
+  {
+    "revision": "e5007e443d540a6252a3",
+    "url": "/cretanTouristGuide/static/js/2.d88cf159.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/cretanTouristGuide/static/js/2.d88cf159.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4dd09d4c367609a34d0",
+    "url": "/cretanTouristGuide/static/js/main.b3a455b6.chunk.js"
+  },
+  {
+    "revision": "9dbfd1c162e358084691",
+    "url": "/cretanTouristGuide/static/js/runtime-main.e81bd814.js"
+  },
+  {
+    "revision": "6ddd03f3658d2873ec90b70440880c93",
+    "url": "/cretanTouristGuide/static/media/Ag.Nikolaos.6ddd03f3.jpg"
+  },
+  {
+    "revision": "da3d9d72d64633f0967d4ab317017c2c",
+    "url": "/cretanTouristGuide/static/media/Chania.da3d9d72.jpg"
+  },
+  {
+    "revision": "80d8fc97ef675e20909a8d5ae097ec7a",
+    "url": "/cretanTouristGuide/static/media/Heraklion.80d8fc97.png"
+  },
+  {
+    "revision": "2bb6673b2a6fad59e2f295299a36f97e",
+    "url": "/cretanTouristGuide/static/media/me.2bb6673b.png"
+  },
+  {
+    "revision": "2552297b7b75d9f2e7f5a5ffb932b16d",
+    "url": "/cretanTouristGuide/static/media/rethymno.2552297b.png"
+  }
+]);
